47e1d00a71c15b13d40e500542d9c61ce59a11a9
ZhaoGuangYue1986 <jeky_zhao@.com>
2021-01-24 10:14:04 +0800

    first version

---
+++ qa-plugin/q2a-open-login-master/qa-open-lang-zh.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+	Question2Answer (c) Gideon Greenspan
+	Open Login Plugin (c) Alex Lixandru
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/open-login/qa-open-lang-default.php
+	Version: 3.0.0
+	Description: Default English translation of all plugin texts
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+
+return array(
+	'my_logins_title' => '我的第三方授权登录信息',
+	'my_logins_nav' => '第三方授权登录信息',
+	'my_current_user' => '当前账号',
+	'associated_logins' => '当前使用的账号',
+	'other_logins' => '使用此邮箱的其他账号',
+	'other_logins_conf_title' => '确认要链接的账号',
+	'other_logins_conf_text' => '我们检测到有其他账户使用了相同的邮箱，您可以将其他账户和当前账户进行绑定，以免重复',
+	'split_accounts_note' => '当前绑定的第三方账户如下，它们只用于授权登录，我们不会将你发布的内容同步到您的社交账户',
+	'no_logins_title' => '你的资料中没有绑定其他的第三方账户',
+	'no_logins_text' => '将当前账户与第三方账户进行绑定',
+	'login_title' => '使用第三方账号登录',
+	'login_description' => '当前支持的第三方登录方式如下：',
+	'login_using' => '推荐使用 ^ 登录',
+	'password' => '密码',
+	'continue' => '继续',
+	'choose_action' => '请选择您要进行的操作:',
+	'merge_all' => '合并所有的账号到一个.',
+	'merge_all_first' => '您所使用的所有账号将合并成一个.',
+	'select_merge' => '请选择您要合并的账号.',
+	'select_merge_first' => '只有选中的账号将会被合并成一个.',
+	'merge_note' => '您可以选择一个主帐户作为合并后的保留账户。 需要注意：合并后他们的信息将被删除。 被合并的账号积分和声誉将不会迁移，并且先前的活动将被标记为匿名。 您选择的主要帐户的活动和积分将保持不变。 此动作不可逆.',
+	'cancel_merge' => '我可以接受重复账户，保留现状',
+	'cancel_merge_note' => '如果选择此选项，则您的信誉点可能会分散在多个用户帐户中。 目前，无法将积分从一个帐户迁移到另一个帐户，这就是为什么建议将所有帐户合并为一个帐户的原因。',
+	'select_base' => '选择一个账户',
+	'select_base_note' => '选择一个合并后的登录主账户:',
+	'current_account' => '当前账户',
+	'action_info_1' => '请选择下一步操作.',
+	'action_info_2' => '未执行任何变更，你的账号仍然保持原样.',
+	'action_info_3' => '请选择一个账户.',
+	'action_info_4' => '此账户将被保留，同时其他的账户将被合并.',
+	'action_info_5' => '没有选择重复的账号，请至少选择一个账户.',
+	'unlink_this_account' => '解绑',
+	'link_with_account' => '将当前账户与其他第三方账户绑定',
+	'link_all' => '将所有账号合并成一个.',
+	'cancel_link' => '取消合并.',
+	'link_exists' => '您绑定的第三方账号( ^) 已经被其他账户绑定. 如果那些账户也是你的，你可以将他们进行合并.',
+);
